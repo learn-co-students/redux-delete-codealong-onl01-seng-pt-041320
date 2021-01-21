c7dbf1662e1df206fbe83c4b30545f9d301dd161
timothyphilip <philippetimothe@gmail.com>
2021-01-21 18:56:18 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,13 +1,57 @@
+// ./src/reducers/manageTodo.js
+import uuid from 'uuid';
+ 
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.payload.text) };
-
+ 
+      const todo = {
+        id: uuid(),
+        text: action.payload.text
+      }
+      return { todos: state.todos.concat(todo) };
+ 
+    case 'DELETE_TODO':
+ 
+      // return {todos: state.todos.filter(todo => todo !== action.payload)}
+      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
+ 
     default:
       return state;
   }
 }
+
+// export default function manageTodo(state = {
+//   todos: [],
+// }, action) {
+//   console.log(action);
+//   switch (action.type) {
+//     case 'ADD_TODO':
+ 
+//       return { todos: state.todos.concat(action.payload.text) };
+ 
+//     case 'DELETE_TODO':
+ 
+//       return {todos: state.todos.filter(todo => todo !== action.payload)}
+ 
+//     default:
+//       return state;
+//   }
+// }
+
+// export default function manageTodo(state = {
+//   todos: [],
+// }, action) {
+//   console.log(action)
+//   switch (action.type) {
+//     case 'ADD_TODO':
+
+//       return { todos: state.todos.concat(action.payload.text) };
+
+//     default:
+//       return state;
+//   }
+// }
